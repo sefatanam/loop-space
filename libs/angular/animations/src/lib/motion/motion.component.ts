236b3ae1d5e7fa1a42f8327b36724887db59c74b
sefatanam <sefatanam@gmail.com>
2025-06-01 01:23:28 +0600

    feat(motion): enhance motion component with search overlay and panel animations
    - Added a new CSS file for shake animation.
    - Updated HTML to include a search overlay with animation and a styled panel with gradient background.
    - Introduced new toggle functions for search and welcome panels in the TypeScript component.

--- libs/angular/animations/src/lib/motion/motion.component.ts
+++ libs/angular/animations/src/lib/motion/motion.component.ts
@@ -6,14 +6,25 @@ import { MotionDirective } from "@loop-space/directives";
 	selector: "lan-motion",
 	imports: [CommonModule, MotionDirective],
 	templateUrl: "./motion.component.html",
+	styleUrl: './motion.component.css',
 	host: {
 		class: "block w-92",
 	},
 })
 export class MotionComponent {
 	isPanelOpen = false;
+	isSearchOpen = false;
+	isWelcomeOpen = false;
 
 	togglePanel() {
 		this.isPanelOpen = !this.isPanelOpen;
+	}
+
+	toggleSearch() {
+		this.isSearchOpen = !this.isSearchOpen;
+	}
+
+	toggleWelcome() {
+		this.isWelcomeOpen = !this.isWelcomeOpen;
 	}
 }
