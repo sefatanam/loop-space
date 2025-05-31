8e75eef34d48c694823bc2513088245e1ea22a26
sefatanam <sefatanam@gmail.com>
2025-06-01 02:38:14 +0600

    feat(highlight): integrate highlight.js and ngx-highlightjs for code highlighting
    - Added highlight.js and ngx-highlightjs dependencies to package.json.
    - Configured highlight options in app.config.ts for dynamic loading.
    - Updated styles.css to include highlight.js theme.
    - Enhanced app.component.html to display highlighted code snippets.
    - Improved motion.component.html with additional animation examples.

--- sandbox/angular-animations/src/app/app.component.ts
+++ sandbox/angular-animations/src/app/app.component.ts
@@ -2,14 +2,41 @@ import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { NavData } from "./route-items.data";
 import { ButtonDirective } from "@loop-space/shared-ui";
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
+import 'highlight.js/styles/github-dark.min.css';
 
 @Component({
-	imports: [RouterModule, ButtonDirective],
+	imports: [RouterModule, ButtonDirective, Highlight, HighlightLineNumbers],
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.css",
 })
 export class AppComponent {
+
+	codeForHighlight = `
+	<div class="container mx-auto px-4 py-8">
+		<h2 class="text-3xl font-bold text-gray-800 mb-4">Basic Panel Animation</h2>
+		<p class="text-gray-600 mb-6">This section demonstrates a simple height and opacity animation for a collapsible
+			panel with customizable background color.</p>
+		<div class="mb-12 p-6 border border-gray-300 rounded-lg shadow-lg bg-white">
+			<div>
+				<div libMotion style="overflow: hidden; background-color: #f0f0f0; padding: 10px"
+					[initial]="{ height: '0px', opacity: 0, backgroundColor: 'red' }"
+					[animate]="{ height: '200px', opacity: 1, backgroundColor: 'blue' }"
+					[exit]="{ height: '0px', opacity: 0 }" [variant]="isPanelOpen ? 'animate' : 'initial'"
+					[duration]="'.5s'" [easing]="'cubic-bezier(0.68, -0.6, 0.32, 1.6)'">
+					<p>This is the content inside the panel!</p>
+				</div>
+
+				<button (click)="togglePanel()">
+					Toggle Panel
+				</button>
+			</div>
+		</div>
+	</div>
+`;
 	navItems: Readonly<NavData[]> = [
 		{
 			url: "basic",
@@ -19,17 +46,17 @@ export class AppComponent {
 			url: "motion",
 			title: "Motion",
 		},
-		// {
-		// 	url: "enable-disable",
-		// 	title: "Enable / Disable Animation",
-		// },
-		// {
-		// 	url: "enter-leave",
-		// 	title: "Enter / Leave Animation",
-		// },
-		// {
-		// 	url: "keyframe",
-		// 	title: "Keyframe Animation",
-		// },
+		/* {
+		url: "enable-disable",
+		title: "Enable / Disable Animation",
+		},
+		{
+		url: "enter-leave",
+		title: "Enter / Leave Animation",
+		},
+		{
+		url: "keyframe",
+		title: "Keyframe Animation",
+		}, */
 	];
 }
