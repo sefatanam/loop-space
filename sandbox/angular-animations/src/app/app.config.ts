031f6fd4b12d39dc53646e3b5b6af538efde2599
sefatanam <sefatanam@gmail.com>
2025-06-01 02:38:14 +0600

    feat(highlight): integrate highlight.js and ngx-highlightjs for code highlighting
    - Added highlight.js and ngx-highlightjs dependencies to package.json.
    - Configured highlight options in app.config.ts for dynamic loading.
    - Updated styles.css to include highlight.js theme.
    - Enhanced app.component.html to display highlighted code snippets.
    - Improved motion.component.html with additional animation examples.

--- sandbox/angular-animations/src/app/app.config.ts
+++ sandbox/angular-animations/src/app/app.config.ts
@@ -2,10 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import appRoutes from "./app.routes";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideHighlightOptions } from 'ngx-highlightjs';
+import { provideHttpClient, withFetch } from "@angular/common/http";
+
 export const appConfig: ApplicationConfig = {
 	providers: [
+		provideHttpClient(withFetch()),
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(appRoutes),
 		provideAnimationsAsync(),
+		provideHighlightOptions(
+			{
+				fullLibraryLoader: () => import('highlight.js'),
+				lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
+			}
+		)
 	],
 };
